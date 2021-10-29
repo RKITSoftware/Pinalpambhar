4d3402c3eed252238b4a9886b9ac2173f42f99c2
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:39:11 +0530

    protectedAccessModifier_Demo

---
+++ Module-3/Code/AccessModifiers/protectedAccessModifier/protectedAccessModifier/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace protectedAccessModifier
+{
+    class X
+    {
+
+        // Member x declared
+        // as protected
+        protected int x;
+
+        public X()
+        {
+            x = 10;
+        }
+    }
+
+    // class Y inherits the
+    // class X
+    class Y : X
+    {
+
+        // Members of Y can access 'x'
+        public int getX()
+        {
+            return x;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            X obj1 = new X();
+            Y obj2 = new Y();
+
+            // Displaying the value of x
+            Console.WriteLine("Value of x is : {0}", obj2.getX());
+
+            // Error: inaccessible due to its protection level
+           // Console.WriteLine("Value of x is : {0}", obj1.x);
+        }
+    }
+}
