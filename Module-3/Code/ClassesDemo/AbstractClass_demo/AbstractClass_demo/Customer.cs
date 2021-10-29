70d945ec8b00740e79df91afc55c7628396b8164
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:42:15 +0530

    AbstractClass_demo

---
+++ Module-3/Code/ClassesDemo/AbstractClass_demo/AbstractClass_demo/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractClass_demo
+{
+    public abstract class Customer
+    {
+        private string _firstName;
+        private string _lastName;
+
+        //get and  is used return variable's value 
+        //set is used to assign a value to variable
+
+        #region Property : _firstName
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+
+            set
+            {
+                _firstName = value;
+            }
+        }
+        #endregion Property : _firstName
+
+        #region Property : _lastName
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+        #endregion Property : _lastName
+
+       //Abstract Method FullName
+        public abstract void FullName();
+    }
+}
---
+++ Module-3/Code/ClassesDemo/AbstractClass_demo/AbstractClass_demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using AbstractClass_demo;
+using System;
+
+namespace AbstractClass_demo
+{
+    class Program: Customer
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.FirstName = "Pinal";
+            p.LastName = "Pambhar";
+            p.FullName();
+        }
+
+        public override void FullName()
+        {
+            Console.WriteLine("Full Name:" + FirstName + " " + LastName);
+        }
+    }
+}
