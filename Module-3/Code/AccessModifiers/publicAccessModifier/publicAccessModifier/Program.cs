b5a4513467a9edc8dedf2f9d8653a74a34459c85
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:40:37 +0530

    publicAccessModifier_Demo

---
+++ Module-3/Code/AccessModifiers/publicAccessModifier/publicAccessModifier/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace publicAccessModifier
+{
+    class Program
+    {
+        class PublicAccessMod
+        {
+            public int num1;
+        }
+        static void Main(string[] args)
+        {
+            PublicAccessMod obj1 = new PublicAccessMod();
+            //Direct access to public members  
+            obj1.num1 = 100;
+            Console.WriteLine("Number one value in main {0}", obj1.num1);
+        }
+    }
+}
