5562700956690a859fd1deb1f54d20bc6cd52b43
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:37:20 +0530

    privateAccessModifier_Demo

---
+++ Module-3/Code/AccessModifiers/privateAccessModifier/privateAccessModifier/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace privateAccessModifier
+{
+    class Program
+    {
+        class PrivateAccessMod
+        {
+            public int num1;
+            int num2;
+        }
+        static void Main(string[] args)
+        {
+            PrivateAccessMod ob1 = new PrivateAccessMod();
+            //Direct access to public members  
+            ob1.num1 = 100;
+            //Access to private member is not permitted  
+            ob1.num2 = 20;
+            Console.WriteLine("Number one value in main {0}", ob1.num1);
+            Console.ReadLine();
+        }
+    }
+}
