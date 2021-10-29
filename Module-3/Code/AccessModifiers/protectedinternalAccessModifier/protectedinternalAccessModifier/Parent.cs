4ead08e166a1c236feb0fe13000f80a4ed195052
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:39:58 +0530

    protectedinternalAccessModifier_Demo

---
+++ Module-3/Code/AccessModifiers/protectedinternalAccessModifier/protectedinternalAccessModifier/Parent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace protectedinternalAccessMod
+{
+    public class Parent
+    {
+
+        // Declaring member as protected internal
+        protected internal int value;
+    }
+
+    class ABC
+    {
+
+        // Trying to access
+        // value in another class
+        public void testAccess()
+        {
+            // Member value is Accessible
+            Parent obj1 = new Parent();
+            obj1.value = 12;
+        }
+    }
+}
---
+++ Module-3/Code/AccessModifiers/protectedinternalAccessModifier/protectedinternalAccessModifier/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using protectedinternalAccessMod;
+
+namespace protectedinternalAccessModifier
+{
+    class abc
+    {
+        Parent objp1 = new Parent();
+        //not accessible in different assembly and non-derived class
+        //objp1.value = 9;
+
+    }
+    class Program: Parent
+    {
+        static void Main(string[] args)
+        {
+            // Accessing value in another assembly only for derived class
+            Program obj3 = new Program();
+
+            // Member value is Accessible
+            obj3.value = 9;
+            Console.WriteLine("Value = " + obj3.value);
+        }
+    }
+}
