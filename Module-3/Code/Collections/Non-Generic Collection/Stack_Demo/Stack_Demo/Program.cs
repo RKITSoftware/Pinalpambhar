49bd664a99885d384e21c9fb3edd280252c3cfac
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:54:38 +0530

    Stack_Demo

---
+++ Module-3/Code/Collections/Non-Generic Collection/Stack_Demo/Stack_Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace Stack_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes a new Stack.
+            Stack stk = new Stack();
+            stk.Push("cs.net");
+            stk.Push("vb.net");
+            stk.Push("asp.net");
+            stk.Push("sqlserver");
+
+            // Displays the properties and values of the Stack.
+            Console.WriteLine("Count:    {0}", stk.Count);
+            Console.Write("Values:\t");
+            PrintValues(stk);
+
+            //remove element from the stack
+            Console.Write("Values:\t");
+            stk.Pop();
+            PrintValues(stk);
+        }
+
+        #region Method:PrintValues
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+            {
+                Console.Write("{0}  ", obj);
+            }
+            Console.WriteLine();
+        }
+        #endregion Method:PrintValues
+    }
+}
