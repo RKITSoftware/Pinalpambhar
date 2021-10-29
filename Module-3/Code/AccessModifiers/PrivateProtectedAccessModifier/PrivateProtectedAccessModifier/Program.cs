5dd1eb14695113b82094ab7084d70e67e88cbe50
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:38:22 +0530

    PrivateProtectedAccessModifier_Demo

---
+++ Module-3/Code/AccessModifiers/PrivateProtectedAccessModifier/PrivateProtectedAccessModifier/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PrivateProtectedAccessModifier
+{
+    class Parent
+    {
+        // Member is declared as private protected
+        private protected int value;
+
+        // value is Accessible only inside the class
+        public void setValue(int v)
+        {
+            value = v;
+        }
+        public int getValue()
+        {
+            return value;
+        }
+    }
+
+    class Child : Parent
+    {
+        public void showValue()
+        {
+            // Trying to access value
+            // Inside a derived class
+
+            Console.WriteLine("Value = " + value);
+            // value is accessible
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent obj = new Parent();
+
+            // obj.value = 5;
+            // Also gives an error
+
+            // Use public functions to assign
+            // and use value of the member 'value'
+            obj.setValue(4);
+            Console.WriteLine("Value = " + obj.getValue());
+        }
+    }
+}
