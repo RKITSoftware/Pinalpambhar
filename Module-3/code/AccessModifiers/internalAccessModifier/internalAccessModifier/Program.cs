8c93fba9a8f8d9181d8e903209401dd0c7447326
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:28:01 +0530

    Internal_AccessModifier_Demo

---
+++ Module-3/code/AccessModifiers/internalAccessModifier/internalAccessModifier/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace internalAccessModifier
+{
+    // Declare class Complex as internal
+    public class Complex
+    {
+
+        internal int real;
+        internal int img;
+
+        public void setData(int r, int i)
+        {
+            real = r;
+            img = i;
+        }
+
+        public void displayData()
+        {
+            Console.WriteLine("Real = {0}", real);
+            Console.WriteLine("Imaginary = {0}", img);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the class Complex
+            // in separate class but within
+            // the same assembly
+            Complex c = new Complex();
+
+            // Accessible in class Program
+            c.setData(2, 1);
+            c.displayData();
+        }
+    }
+}
