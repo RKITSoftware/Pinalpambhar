48fc8ec728bd8301c415c3ca0596597b4bfee09a
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:43:54 +0530

    SealedClass_Demo

---
+++ Module-3/Code/ClassesDemo/SealedClass_Demo/SealedClass_Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SealedClass_Demo
+{
+    // Sealed class
+    sealed class SealedClass
+    {
+
+        // Addition Method
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    //class Program: SealedClass; this cannot be done
+    class Program  
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of Sealed Class
+            SealedClass slc = new SealedClass();
+
+            // Performing Addition operation
+            int total = slc.Add(6, 4);
+            Console.WriteLine("Total = " + total.ToString());
+        }
+    }
+}
