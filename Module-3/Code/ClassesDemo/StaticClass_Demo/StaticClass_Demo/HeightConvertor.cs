4b232b9cd781d6d3eadd294c2d9debf62251a70a
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:44:30 +0530

    StaticClass_Demo

---
+++ Module-3/Code/ClassesDemo/StaticClass_Demo/StaticClass_Demo/HeightConvertor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticClass_Demo
+{
+    public static class HeightConvertor
+    {
+
+        #region Method: InchesToCentimeters
+
+        /*static method InchesToCentimeters() to convert Height in Inches to Centimeters
+         this method takes height in Inches as a parameter
+         and return centimeters as an output*/
+        public static double InchesToCentimeters(string HeightInInches)
+        {
+            double inches = Double.Parse(HeightInInches);
+            double Centimeters = (inches * 2.54);
+            return Centimeters;
+        }
+        #endregion Method: InchesToCentimeters
+
+        #region Method: CentimetesToInches
+        /*static method CentimetesToInches to convert Height in Centimeters to Inches
+         this method takes height in Centimeters as a parameter
+         and return Inches as an output*/
+        public static double CentimetesToInches(string HeightInCentimeters)
+        {
+            double centimeters = Double.Parse(HeightInCentimeters);
+            double Inches = (centimeters / 2.54);
+            return Inches;
+        }
+        #endregion Method: CentimetesToInches
+    }
+}
---
+++ Module-3/Code/ClassesDemo/StaticClass_Demo/StaticClass_Demo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StaticClass_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please select the convertor direction");
+            Console.WriteLine("1. From Inches to Centimeters.");
+            Console.WriteLine("2. From Centimeters to Inches.");
+
+            string selection = Console.ReadLine();
+            double C, I = 0;
+
+            switch (selection)
+            {
+                case "1":
+                    Console.Write("Please enter the height in inches: ");
+                    C = HeightConvertor.InchesToCentimeters(Console.ReadLine());
+                    Console.WriteLine("Hieght in centimeters: {0:F2}", C);
+                    break;
+
+                case "2":
+                    Console.Write("Please enter the Height in centimeters: ");
+                    I = HeightConvertor.CentimetesToInches(Console.ReadLine());
+                    Console.WriteLine("Height in Inchs: {0:F2}", I);
+                    break;
+
+                default:
+                    //read value is lable will ask for valid value
+                    ReadValue:
+                        Console.WriteLine("Please select a valid convertor.");
+                        selection = Console.ReadLine();
+
+                    if (selection == "1") //if selection is 1 control will pass to case 1
+                    {
+                        goto case "1";
+                    }     
+                    else if (selection == "2") //if selection is 2 control will pass to case 2
+                    {
+                        goto case "2";
+                    }
+                    else //otherwise ask for a valid value
+                    {
+                        goto ReadValue;
+                    }
+            }
+        }
+    }
+}
