5c5864541eff40a16eeca9e69329310b7e95e8c5
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-10-29 22:53:48 +0530

    Queue_Demo

---
+++ Module-3/Code/Collections/Non-Generic Collection/Queue_Demo/Queue_Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace Queue_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates and initializes a new Queue.
+            Queue q = new Queue();
+            q.Enqueue("cs.net");
+            q.Enqueue("vb.net");
+            q.Enqueue("asp.net");
+            q.Enqueue("sqlserver");
+
+            // Displays the properties and values of the Queue.
+            Console.WriteLine("Count:    {0}", q.Count);
+            Console.Write("Values:");
+            PrintValues(q);
+
+            //peek returns the oldest element that is at the start of the 
+            //Queue but does not remove
+            Console.WriteLine("oldest element:    {0}", q.Peek());
+
+            //Dequeue removes the oldest element from the start of the Queue.
+            q.Dequeue();
+
+            Console.Write("Values:");
+            PrintValues(q);
+        }
+
+        #region Method:PrintValues
+        public static void PrintValues(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+            {
+                Console.Write("{0}  ", obj);
+            }
+            Console.WriteLine();
+        }
+        #endregion Method:PrintValues
+    }
+}
