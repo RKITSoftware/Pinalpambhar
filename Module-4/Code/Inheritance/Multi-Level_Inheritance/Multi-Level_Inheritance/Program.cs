a862efeefa36685545e592f68c1e12c03c5d57a9
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:31:45 +0530

    Multi-Level_Inheritance

---
+++ Module-4/Code/Inheritance/Multi-Level_Inheritance/Multi-Level_Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Multi_Level_Inheritance
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating..."); 
+        }
+    }
+    public class Dog : Animal
+    {
+        public void bark() 
+        {
+            Console.WriteLine("Barking..."); 
+        }
+    }
+    public class BabyDog : Dog
+    {
+        public void weep() 
+        {
+            Console.WriteLine("Weeping...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BabyDog d1 = new BabyDog();
+            d1.eat();
+            d1.bark();
+            d1.weep();
+        }
+    }
+}
+/* Output
+ * Eating...
+ * Barking...
+ * Weeping...
+*/
