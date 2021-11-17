d5695c2c0621c47f3e433e2267b1df5a81f3d3fb
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:15:24 +0530

    Enumeration_Demo

---
+++ Module-4/Code/Enumerations/Enumerations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Enumerations
+{
+    class Program
+    {
+        // making an enumerator 'month'
+        public enum month
+        {
+
+            // following are the data members
+            jan = 1,
+            feb = 2,
+            mar = 3,
+            apr = 4,
+            may = 5
+
+        }
+        static void Main(string[] args)
+        {
+            // getting the integer values of data members..
+            Console.WriteLine("The value of jan in month enum is " + (int)month.jan);
+            Console.WriteLine("The value of feb in month enum is " + (int)month.feb);
+            Console.WriteLine("The value of mar in month enum is " + (int)month.mar);
+            Console.WriteLine("The value of apr in month enum is " + (int)month.apr);
+            Console.WriteLine("The value of may in month enum is " + (int)month.may);
+
+            /*Output
+             * The value of jan in month enum is 1
+               The value of feb in month enum is 2
+               The value of mar in month enum is 3
+               The value of apr in month enum is 4
+               The value of may in month enum is 5
+            */
+
+        }
+    }
+}
