c4dbb0f64db8a13382928cbea03075a2f6327b1c
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:17:54 +0530

    Event_Demo

---
+++ Module-4/Code/Event/WindowsFormsApp_Calculator/FormCalculator.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp_Calculator
+{
+    partial class FormCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.lblNumber1 = new System.Windows.Forms.Label();
+            this.lblNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btnSubtraction = new System.Windows.Forms.Button();
+            this.btnMultiplication = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.btnModulo = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnAddition = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(46, 66);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(259, 20);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // lblNumber1
+            // 
+            this.lblNumber1.AutoSize = true;
+            this.lblNumber1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber1.Location = new System.Drawing.Point(42, 33);
+            this.lblNumber1.Name = "lblNumber1";
+            this.lblNumber1.Size = new System.Drawing.Size(145, 21);
+            this.lblNumber1.TabIndex = 2;
+            this.lblNumber1.Text = "Enter 1st Number";
+            // 
+            // lblNumber2
+            // 
+            this.lblNumber2.AutoSize = true;
+            this.lblNumber2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber2.Location = new System.Drawing.Point(42, 110);
+            this.lblNumber2.Name = "lblNumber2";
+            this.lblNumber2.Size = new System.Drawing.Size(152, 21);
+            this.lblNumber2.TabIndex = 3;
+            this.lblNumber2.Text = "Enter 2nd Number";
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(46, 145);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(259, 20);
+            this.txtNumber2.TabIndex = 4;
+            // 
+            // btnSubtraction
+            // 
+            this.btnSubtraction.BackColor = System.Drawing.Color.Aqua;
+            this.btnSubtraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtraction.Location = new System.Drawing.Point(132, 189);
+            this.btnSubtraction.Name = "btnSubtraction";
+            this.btnSubtraction.Size = new System.Drawing.Size(62, 46);
+            this.btnSubtraction.TabIndex = 5;
+            this.btnSubtraction.Text = "-";
+            this.btnSubtraction.UseVisualStyleBackColor = false;
+            this.btnSubtraction.Click += new System.EventHandler(this.btnSubtraction_Click);
+            // 
+            // btnMultiplication
+            // 
+            this.btnMultiplication.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnMultiplication.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiplication.Location = new System.Drawing.Point(223, 189);
+            this.btnMultiplication.Name = "btnMultiplication";
+            this.btnMultiplication.Size = new System.Drawing.Size(62, 46);
+            this.btnMultiplication.TabIndex = 6;
+            this.btnMultiplication.Text = "*";
+            this.btnMultiplication.UseVisualStyleBackColor = false;
+            this.btnMultiplication.Click += new System.EventHandler(this.btnMultiplication_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(46, 254);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(62, 46);
+            this.btnDivision.TabIndex = 7;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = false;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // btnModulo
+            // 
+            this.btnModulo.BackColor = System.Drawing.Color.Green;
+            this.btnModulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModulo.Location = new System.Drawing.Point(132, 254);
+            this.btnModulo.Name = "btnModulo";
+            this.btnModulo.Size = new System.Drawing.Size(62, 46);
+            this.btnModulo.TabIndex = 8;
+            this.btnModulo.Text = "%";
+            this.btnModulo.UseVisualStyleBackColor = false;
+            this.btnModulo.Click += new System.EventHandler(this.btnModulo_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(42, 327);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(66, 21);
+            this.lblAnswer.TabIndex = 9;
+            this.lblAnswer.Text = "Answer";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(35, 360);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(259, 20);
+            this.txtAnswer.TabIndex = 10;
+            // 
+            // btnAddition
+            // 
+            this.btnAddition.BackColor = System.Drawing.Color.MediumSpringGreen;
+            this.btnAddition.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddition.Location = new System.Drawing.Point(46, 189);
+            this.btnAddition.Name = "btnAddition";
+            this.btnAddition.Size = new System.Drawing.Size(62, 46);
+            this.btnAddition.TabIndex = 11;
+            this.btnAddition.Text = "+";
+            this.btnAddition.UseVisualStyleBackColor = false;
+            this.btnAddition.Click += new System.EventHandler(this.btnAddition_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Red;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(223, 254);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(62, 46);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // FormCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(365, 405);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnAddition);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnModulo);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnMultiplication);
+            this.Controls.Add(this.btnSubtraction);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.lblNumber2);
+            this.Controls.Add(this.lblNumber1);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "FormCalculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.Label lblNumber1;
+        private System.Windows.Forms.Label lblNumber2;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btnSubtraction;
+        private System.Windows.Forms.Button btnMultiplication;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.Button btnModulo;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnAddition;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Module-4/Code/Event/WindowsFormsApp_Calculator/FormCalculator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+using ClassLibrary_Calculator;
+
+namespace WindowsFormsApp_Calculator
+{
+    public partial class FormCalculator : Form
+    {
+        #region Constructor
+        public FormCalculator()
+        {
+            InitializeComponent();
+        }
+        #endregion Constructor
+
+        #region objects_declaration
+        Calculator objcalculator = new Calculator();
+        #endregion objects_declaration
+
+        #region method_btnAddition
+        private void btnAddition_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumber1.Text) || string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                txtAnswer.Text = "Number can't be blank";
+            }
+            else if (Convert.ToDouble(txtNumber1.Text) < 0 || Convert.ToDouble(txtNumber2.Text) < 0)
+            {
+                txtAnswer.Text = "Invalid Input";
+            }
+            else
+            {
+                double answer;
+                lblAnswer.Text = "(" + txtNumber1.Text.ToString().Trim() + " + " + txtNumber2.Text.ToString().Trim() + ")";
+                answer = objcalculator.Addition(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+                txtAnswer.Text = Convert.ToString(answer);
+            }
+        }
+        #endregion method_btnAddition
+
+        #region method_btnSubtraction
+        private void btnSubtraction_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumber1.Text) || string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                txtAnswer.Text = "Number can't be blank";
+            }
+            else if (Convert.ToDouble(txtNumber1.Text) < 0 || Convert.ToDouble(txtNumber2.Text) < 0)
+            {
+                txtAnswer.Text = "Invalid Input";
+            }
+            else
+            {
+                double answer;
+                lblAnswer.Text = "(" + txtNumber1.Text.ToString().Trim() + " - " + txtNumber2.Text.ToString().Trim() + ")";
+                answer = objcalculator.Subtraction(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+                txtAnswer.Text = Convert.ToString(answer);
+            }
+        }
+        #endregion method_btnSubtraction
+
+        #region method_btnClear
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNumber1.Text = String.Empty;
+            txtNumber2.Text = String.Empty;
+            txtAnswer.Text = String.Empty;
+            lblAnswer.Text = String.Empty;
+        }
+        #endregion method_btnClear
+
+        #region method_btnMultiplication
+        private void btnMultiplication_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumber1.Text) || string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                txtAnswer.Text = "Number can't be blank";
+            }
+            else if (Convert.ToDouble(txtNumber1.Text) < 0 || Convert.ToDouble(txtNumber2.Text) < 0)
+            {
+                txtAnswer.Text = "Invalid Input";
+            }
+            else
+            {
+                double answer;
+                lblAnswer.Text = "(" + txtNumber1.Text.ToString().Trim() + " * " + txtNumber2.Text.ToString().Trim() + ")";
+                answer = objcalculator.Multiplication(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+                txtAnswer.Text = Convert.ToString(answer);
+            }
+        }
+        #endregion method_btnMultiplication
+
+        #region method_btnDivision
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumber1.Text) || string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                txtAnswer.Text = "Number can't be blank";
+            }
+            else if (Convert.ToDouble(txtNumber1.Text) < 0 || Convert.ToDouble(txtNumber2.Text) < 0)
+            {
+                txtAnswer.Text = "Invalid Input";
+            }
+            else
+            {
+                double answer;
+                lblAnswer.Text = "(" + txtNumber1.Text.ToString().Trim() + " / " + txtNumber2.Text.ToString().Trim() + ")";
+                answer = objcalculator.Division(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+                txtAnswer.Text = Convert.ToString(answer);
+            }
+        }
+        #endregion method_btnDivision
+
+        #region method_btnModulo
+        private void btnModulo_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNumber1.Text) || string.IsNullOrEmpty(txtNumber2.Text))
+            {
+                txtAnswer.Text = "Number can't be blank";
+            }
+            else if (Convert.ToDouble(txtNumber1.Text) < 0 || Convert.ToDouble(txtNumber2.Text) < 0)
+            {
+                txtAnswer.Text = "Invalid Input";
+            }
+            else
+            {
+                double answer;
+                lblAnswer.Text = "(" + txtNumber1.Text.ToString().Trim() + " % " + txtNumber2.Text.ToString().Trim() + ")";
+                answer = objcalculator.Modulo(Convert.ToDouble(txtNumber1.Text), Convert.ToDouble(txtNumber2.Text));
+                txtAnswer.Text = Convert.ToString(answer);
+            }
+        }
+        #endregion method_btnModulo
+    }
+}
