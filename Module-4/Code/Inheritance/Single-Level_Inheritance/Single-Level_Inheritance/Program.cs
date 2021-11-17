bb0b1360f9c746edfd39d2aa8538f2f877e0ab81
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:33:51 +0530

    Single-Level_Inheritance

---
+++ Module-4/Code/Inheritance/Single-Level_Inheritance/Single-Level_Inheritance/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Single_Level_Inheritance
+{
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer p1 = new Programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+        }
+    }
+}
+/* Output
+ * Salary: 40000
+ * Bonus: 10000
+*/
