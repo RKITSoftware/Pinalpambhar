e33f4aec867d53542b98886c936b592c0328d3e1
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:30:18 +0530

    Hierarchical-Inheritance

---
+++ Module-4/Code/Inheritance/Hierarchical-Inheritance/Hierarchical-Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Hierarchical_Inheritance
+{
+    // Base Class
+    public class Father
+    {
+        public string FatherName()
+        {
+            return "Ravi";
+        }
+    }
+
+    // Derived Class
+    public class ChildFirst : Father
+    {
+        public string ChildDName()
+        {
+            return "Rohan";
+        }
+    }
+
+    // Derived Class
+    public class ChildSecond : Father
+    {
+        public string ChildDName()
+        {
+            return "Nikhil";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChildFirst first = new ChildFirst();
+
+            // Displaying Child Name and Father Name for
+            // ChildFirst
+            Console.WriteLine("My name is " + first.ChildDName() + ". My father name is " + first.FatherName() + ".");
+
+            ChildSecond second = new ChildSecond();
+
+            // Displaying Child Name and Father Name for
+            // ChildSecond
+            Console.WriteLine("My name is " + second.ChildDName() + ". My father name is " + second.FatherName() + ".");
+        }
+    }
+}
+/* Output
+ * My name is Rohan. My father name is Ravi.
+ * My name is Nikhil. My father name is Ravi.
+*/
