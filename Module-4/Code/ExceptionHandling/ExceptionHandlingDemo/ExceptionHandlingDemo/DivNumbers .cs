2742480b867c8735272901a241f4bc73a273fc5b
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-17 21:20:35 +0530

    ExceptionHandling_Demo

---
+++ Module-4/Code/ExceptionHandling/ExceptionHandlingDemo/ExceptionHandlingDemo/DivNumbers .cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ExceptionHandlingDemo
+{
+    class DivNumbers
+    {
+        int result;
+
+        //constructor for set result value
+        DivNumbers()
+        {
+            result = 0;
+        }
+
+        //Method for divide numbers
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2; //this statement can occurre an exception
+            }
+            //catch handles an exception
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            //finally exceutes wether exception occurres or not
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DivNumbers d = new DivNumbers();
+            d.division(25, 0);
+        }
+    }
+}
+
+/*output
+ * Exception caught: System.DivideByZeroException: Attempted to divide by zero.
+ * Result: 0
+ */
---
+++ Module-4/Code/ExceptionHandling/UserDefinedExceptionDemo/UserDefinedExceptionDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace UserDefinedExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            try
+            {
+                temp.showTemp();
+            }
+            catch (TempIsZeroException e)
+            {
+                Console.WriteLine("TempIsZeroException: {0}", e.Message);
+            }
+        }
+    }
+    public class TempIsZeroException : Exception
+    {
+        // creating user-defined exception
+        public TempIsZeroException(string message) : base(message)
+        {
+        }
+    }
+
+    public class Temperature
+    {
+        int temperature = 0;
+
+        public void showTemp()
+        {
+
+            if (temperature == 0)
+            {
+                throw (new TempIsZeroException("Zero Temperature found"));
+            }
+            else
+            {
+                Console.WriteLine("Temperature: {0}", temperature);
+            }
+        }
+    }
+}
+/*Output:
+ * TempIsZeroException: Zero Temperature found
+ */
