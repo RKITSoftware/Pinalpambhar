5abac2692d50797fa301d1f0876f0833e61d2146
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-11-29 23:49:21 +0530

    FirstMVC_Project

---
+++ Module-5/Code/FirstMVCProject/FirstMVCProject/Controllers/ResultCalculatorController.cs
@@ -0,0 +1,28 @@
+﻿using FirstMVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCProject.Controllers
+{
+    public class ResultCalculatorController : Controller
+    {
+        // GET: ResultCalculator
+        public ActionResult Index()
+        {
+            return View(new ResultCalculator());
+        }
+
+        [HttpPost]
+
+        public ActionResult Index(ResultCalculator c)
+        {
+            c.Total = c.OperatingSystem + c.CPP + c.ComputerNetwork + c.CompilerDesign + c.Math;
+            c.Percentage = (c.Total * 100) / 500;
+            return View(c);
+
+        }
+    }
+}
---
+++ Module-5/Code/FirstMVCProject/FirstMVCProject/Models/ResultCalculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstMVCProject.Models
+{
+    public class ResultCalculator
+    {
+        public int OperatingSystem { get; set; }
+        public int CPP { get; set; }
+        public int ComputerNetwork { get; set; }
+        public int Math { get; set; }
+        public int CompilerDesign { get; set; }
+        public int Total { get; set; }
+        public double Percentage { get; set; }
+    }
+}
