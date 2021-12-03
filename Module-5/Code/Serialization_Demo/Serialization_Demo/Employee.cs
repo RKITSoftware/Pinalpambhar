eb71733ed5040befd4f5fe6dff244d3d1489885f
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-12-03 14:47:49 +0530

    Serialization_Demo

---
+++ Module-5/Code/Serialization_Demo/Serialization_Demo/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Demo
+{
+    [Serializable]
+    class Employee
+    {
+        public int id;
+        public string name;
+
+        public Employee(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+    }
+}
---
+++ Module-5/Code/Serialization_Demo/Serialization_Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\RKIT\Module-5\serial.txt";
+
+            //serialization
+            Employee Objemp = new Employee(1, "Pinal");
+            //file stream for for create or open file
+            FileStream stream = new FileStream(path, FileMode.OpenOrCreate);
+
+            //binaryformatter object created
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            //serializing file stream which contains objemp's data
+            formatter.Serialize(stream, Objemp);
+            stream.Close();
+            Console.WriteLine("File Saved at" + path);
+
+            //deserialization
+            //file stream for for create or open file
+            FileStream stream1 = new FileStream(path, FileMode.OpenOrCreate);
+
+            //binaryformatter object created
+            BinaryFormatter formatter1 = new BinaryFormatter();
+
+            //deserializing file stream which contains objemp's data
+            Employee objemployee = (Employee)formatter1.Deserialize(stream1);
+
+            //printing data of deserialized object
+            Console.WriteLine("id = " + objemployee.id);
+            Console.WriteLine("Name = " + objemployee.name);
+        }
+    }
+}
