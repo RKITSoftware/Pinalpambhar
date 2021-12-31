9f75ae62ea183229422763d91f77497996513137
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2021-12-31 12:44:29 +0530

    HTTP Verbs Demo

---
+++ Module-6/WebApi_HttpVerbs/WebApi_HttpVerbs/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi_HttpVerbs.Models
+{
+    public class Person
+    {
+        public int Id { get; set; } = 0;
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+    }
+}
