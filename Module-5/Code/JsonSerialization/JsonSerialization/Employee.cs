507f78f998a6b35f863259bb52efdca37452dbbd
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2022-01-03 10:23:04 +0530

    JSONSerialization_Demo

---
+++ Module-5/Code/JsonSerialization/JsonSerialization/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonSerialization
+{
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Module-5/Code/JsonSerialization/JsonSerialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JsonSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee objEmp = new Employee()
+            {
+                EmployeeId = 1,
+                Name = "Pinal"
+            };
+
+            // Convert Employee object to JOSN string format
+            string jsonData = JsonConvert.SerializeObject(objEmp);
+            Console.WriteLine(jsonData);
+
+            string json = @"{
+                              'Name': 'Pinal',
+                              'EmployeeId': 1
+                            }";
+            //deserialize JSON Object
+            Employee ObjEmployee = JsonConvert.DeserializeObject<Employee>(json);
+            Console.Write(ObjEmployee.Name);
+        }
+    }
+}
