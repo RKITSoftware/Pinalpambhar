2869c753e84d5fb2841b152591996b44b791bfbd
Pinal Pambhar <61857387+Pinalpambhar@users.noreply.github.com>
2022-01-03 10:21:53 +0530

    XmlSerialization_Demo

---
+++ Module-5/Code/XmlSerialization/XmlSerialization/AddressDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XmlSerialization
+{
+    public class AddressDetails
+    {
+        public int HouseNo { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        public string PoAddress { get; set; }
+    }
+}
---
+++ Module-5/Code/XmlSerialization/XmlSerialization/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace XmlSerialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressDetails details = new AddressDetails();
+            details.HouseNo = 4;
+            details.StreetName = "Ayodhya";
+            details.City = "Rajkot";
+
+            serialize(details);
+
+            XmlSerializer deserializer = new XmlSerializer(typeof(AddressDetails));
+            TextReader reader = new StreamReader(@"D:\XmlSerialization.xml");
+            object obj = deserializer.Deserialize(reader);
+            AddressDetails XmlData = (AddressDetails)obj;
+            reader.Close();
+
+        }
+
+        static public void serialize(AddressDetails details)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(AddressDetails));
+            using (TextWriter writer = new StreamWriter(@"D:\XmlSerialization.xml"))
+            {
+                serializer.Serialize(writer, details);
+            }
+
+        }
+    }
+}
